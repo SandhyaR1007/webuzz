3535eacad6fb80867a973bc132952003bf4b6395
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-15 00:07:41 +0530

    add: imports for components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./layout/Navbar";
+export { default as Menubar } from "./layout/Menubar";
+export { default as SharedLayout } from "./layout/SharedLayout";
+export { default as SuggestedUsers } from "./SuggestedUsers";
