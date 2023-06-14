bdd75ecc06132e97c9cc085886da68520e8cf39f
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-15 00:07:54 +0530

    add: SharedLayout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SharedLayout } from "./components";
+import { Home } from "./pages";
+
+const App = () => {
+  return (
+    <div>
+      <SharedLayout>
+        <h1>hello</h1>
+      </SharedLayout>
+    </div>
+  );
+};
+
+export default App;
