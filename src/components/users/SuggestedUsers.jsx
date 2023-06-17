f338fea25ad5462d0e3cd5f942f4a4a1ed0e0f3a
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-17 16:46:50 +0530

    add: UsersCard component and display suggested users dynamically

--- src/components/users/SuggestedUsers.jsx
+++ src/components/users/SuggestedUsers.jsx
@@ -1,35 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { usersSelector } from "../../app/features/usersSlice";
+import { UsersCard } from "./UsersCard";
 
-const UsersCard = () => {
-  return (
-    <div className="flex items-center justify-between ">
-      <div className="flex items-center gap-2">
-        <img
-          src="https://pbs.twimg.com/media/FyjuLVNaAAA9vjA?format=jpg&name=900x900"
-          alt="jk"
-          className="w-10 h-10 rounded-lg border border-black"
-        />
-        <div className="">
-          <h1 className="font-semibold">Jeon Jungkook</h1>
-          <h3 className="text-sm text-gray-500">@jungook.97</h3>
-        </div>
-      </div>
-      <button
-        className={`bg-[--pink-color]  text-sm px-3 py-0.5 rounded-full border border-black`}
-      >
-        Follow
-      </button>
-    </div>
-  );
-};
-const arr = [1, 2, 3, 4, 5];
 const SuggestedUsers = () => {
+  const { currentUserId, usersData } = useSelector(usersSelector);
+  const currentUser = usersData.find(({ _id }) => _id === currentUserId);
+  const suggestedUsers = usersData.filter(
+    ({ _id }) =>
+      _id !== currentUserId &&
+      currentUser.following.every((data) => data._id !== _id)
+  );
+
   return (
     <div className="fixed  max-h-screen w-80 pb-10 border border-black  px-5 rounded-lg">
       <h1 className="text-2xl  py-5 font-secondary">Who to follow</h1>
       <div className="flex flex-col gap-3">
-        {arr.map((data) => (
-          <UsersCard />
+        {suggestedUsers.map((data) => (
+          <UsersCard userData={data} />
         ))}
       </div>
     </div>
---
+++ src/components/users/UsersCard.jsx
@@ -0,0 +1,25 @@
+export const UsersCard = ({ userData }) => {
+  const { firstName, lastName, username, profile } = userData;
+  return (
+    <div className="flex items-center justify-between ">
+      <div className="flex items-center gap-2">
+        <img
+          src={profile}
+          alt={username}
+          className="w-10 h-10 rounded-lg border border-black object-fill"
+        />
+        <div className="">
+          <h1 className="font-semibold">
+            {firstName} {lastName}
+          </h1>
+          <h3 className="text-sm text-gray-500">@{username}</h3>
+        </div>
+      </div>
+      <button
+        className={`bg-[--pink-color]  text-sm px-3 py-0.5 rounded-full border border-black`}
+      >
+        Follow
+      </button>
+    </div>
+  );
+};
