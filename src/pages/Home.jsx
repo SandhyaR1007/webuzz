c2ae3137d0f7fe776d6ce8880eba1fe58cd47815
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-17 13:35:32 +0530

    update: added get posts logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Filters from "../components/filters/Filters";
+import { NewPostCard, PostList, SuggestedUsers } from "../components";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  postsSelector,
+  fetchPosts,
+  loadingSelector,
+  errorSelector,
+} from "../app/features/postsSlice";
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const postsData = useSelector(postsSelector);
+  const loading = useSelector(loadingSelector);
+  const error = useSelector(errorSelector);
+  console.log({ postsData });
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
+  return (
+    <>
+      <section className="w-full lg:w-2/3  px-3">
+        <Filters />
+        <NewPostCard />
+        {loading ? <h3>Loading...</h3> : <PostList posts={postsData} />}
+      </section>
+
+      <section className="relative hidden lg:block w-1/3  ">
+        <SuggestedUsers />
+      </section>
+    </>
+  );
 };
 
 export default Home;
