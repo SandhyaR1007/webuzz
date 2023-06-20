186b6f3eede0e333d0eb9d01c47844cf378f55d8
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-21 00:17:49 +0530

    update: added follow user, routing to profile

--- src/components/users/UsersCard.jsx
+++ src/components/users/UsersCard.jsx
@@ -1,22 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { authSelector } from "../../app/features/authSlice";
+import { followUser } from "../../app/features/usersSlice";
+import { Link } from "react-router-dom";
+
 export const UsersCard = ({ userData }) => {
-  const { firstName, lastName, userhandle, profile } = userData;
+  const dispatch = useDispatch();
+  const { encodedToken } = useSelector(authSelector);
+  const { firstName, lastName, username, profile } = userData;
+
   return (
     <div className="flex items-center justify-between ">
       <div className="flex items-center gap-2">
         <img
           src={profile}
-          alt={userhandle}
-          className="w-10 h-10 rounded-lg border border-black object-fill"
+          alt={username}
+          className="w-10 h-10 rounded-lg border border-black object-cover"
         />
-        <div className="">
-          <h1 className="font-semibold">
+        <div className="flex flex-col">
+          <h1 className="font-semibold ">
             {firstName} {lastName}
           </h1>
-          <h3 className="text-sm text-gray-500">@{userhandle}</h3>
+
+          <Link
+            to={`/userProfile/${userData.username}`}
+            className="text-xs text-gray-400"
+          >
+            @{username}
+          </Link>
         </div>
       </div>
       <button
-        className={`bg-[--pink-color]  text-sm px-3 py-0.5 rounded-full border border-black`}
+        onClick={() => {
+          dispatch(followUser({ encodedToken, userId: userData._id }));
+        }}
+        className={`bg-pink-300  text-sm px-3 py-0.5 rounded-full border border-black btn-shadow text-black`}
       >
         Follow
       </button>
