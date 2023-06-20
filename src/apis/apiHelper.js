8e3d9c79fd2a9246097c083da698c50b485a9fc2
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-20 23:31:15 +0530

    add: getPostsByUsername function

---
+++ src/apis/apiHelper.js
@@ -0,0 +1,14 @@
+import { getPostsByUsernameApi } from "./apiServices";
+
+export const getPostsByUsername = async (token, username) => {
+  try {
+    const response = await getPostsByUsernameApi(token, username);
+    console.log({ response });
+    if (response.status === 200) {
+      return { success: true, data: response.data.posts };
+    }
+  } catch (err) {
+    console.log({ err });
+    return { success: false, err };
+  }
+};
