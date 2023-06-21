83cd5cea85fa3a5b42edd78d756472a164502bb7
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-21 23:42:33 +0530

    update: changes icons and logout button

--- src/components/layout/Menubar.jsx
+++ src/components/layout/Menubar.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from "react";
-import { AiOutlineHome, AiOutlineCompass } from "react-icons/ai";
-import { BiUserCircle, BiLogOut } from "react-icons/bi";
-import { BsBookmark } from "react-icons/bs";
+
+import {
+  AiOutlineHome,
+  AiFillCompass,
+  AiOutlineUser,
+  AiOutlineLogout,
+  AiFillStar,
+} from "react-icons/ai";
+
 import { useNavigate } from "react-router";
 import { authSelector, logoutUser } from "../../app/features/authSlice";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,7 +15,10 @@ import { useDispatch, useSelector } from "react-redux";
 const Menubar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { encodedToken } = useSelector(authSelector);
+  const {
+    encodedToken,
+    foundUser: { username },
+  } = useSelector(authSelector);
   useEffect(() => {
     if (encodedToken === null) {
       setTimeout(() => {
@@ -18,43 +27,45 @@ const Menubar = () => {
     }
   }, [encodedToken]);
   return (
-    <ul className="bg-red-300 fixed bottom-0 md:top-[4.75rem] flex  md:flex-col justify-between md:justify-start gap-0 xs:gap-5   py-5 px-3  xs:ps-16 border xs:border-0 w-full md:w-[25vw] lg:w-[21vw] ">
+    <ul className="bg-amber-400 md:bg-transparent fixed bottom-0 md:top-[4.75rem] flex  md:flex-col justify-between md:justify-start gap-0 xs:gap-5   py-5 px-3  xs:ps-16 border xs:border-0 w-full md:w-[25vw] lg:w-[21vw] ">
       <li
         onClick={() => navigate("/")}
-        className="flex items-center  text-2xl  gap-4  py-1 px-3 cursor-pointer hover:text-emerald-500 transition"
+        className="flex items-center  text-xl  gap-4  py-1 px-3 cursor-pointer hover:text-emerald-500 transition"
       >
         <AiOutlineHome className="text-2xl " />
         <span className="hidden md:block">Home</span>
       </li>
       <li
         onClick={() => navigate("/explore")}
-        className="flex items-center  text-2xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition"
+        className="flex items-center  text-xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition"
       >
         {" "}
-        <AiOutlineCompass className="text-2xl " />
+        <AiFillCompass className="text-2xl " />
         <span className="hidden md:block">Explore</span>
       </li>
-      <li className="flex items-center  text-2xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition">
+      <li className="flex items-center  text-xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition">
         {" "}
-        <BsBookmark className="text-2xl  " />
+        <AiFillStar className="text-xl  " />
         <span className="hidden md:block">Bookmark</span>
       </li>
       <li
-        className="flex items-center  text-2xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition"
-        onClick={() => {}}
+        className="flex items-center  text-xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition"
+        onClick={() => {
+          navigate(`/userProfile/${username}`);
+        }}
       >
         {" "}
-        <BiUserCircle className="text-2xl " />
+        <AiOutlineUser className="text-2xl " />
         <span className="hidden md:block">Profile</span>
       </li>
       <li
-        className="flex items-center  text-2xl  gap-4  py-1   px-3 cursor-pointer hover:text-emerald-500 transition"
+        className="md:absolute bottom-20 flex items-center justify-center  text-xl  gap-3  py-1   px-5 cursor-pointer md:bg-purple-400 btn-light max-w-min transition"
         onClick={() => {
           dispatch(logoutUser());
         }}
       >
         {" "}
-        <BiLogOut className="text-2xl " />
+        <AiOutlineLogout className="text-2xl " />
         <span className="hidden md:block">Logout</span>
       </li>
     </ul>
