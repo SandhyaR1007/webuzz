fe91283ee5406c489730c12082fd514f3eee7133
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-21 23:41:26 +0530

    update: seperated styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "./custom.styles.css";
+import "./styles/custom.styles.css";
 import { Index as Routes } from "./routes/index";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, usersSelector } from "./app/features/usersSlice";
