16dbc1d32bb0f45a742d94c909a2f4813e900910
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-23 23:57:39 +0530

    fix: filter to be based on createdAt

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,10 +1,10 @@
 import moment from "moment";
 
 export const sortByLatest = (postsData) =>
-  [...postsData].sort((a, b) => moment(b.updatedAt) - moment(a.updatedAt));
+  [...postsData].sort((a, b) => moment(b.createdAt) - moment(a.createdAt));
 
 export const sortByOldest = (postsData) =>
-  [...postsData].sort((a, b) => moment(a.updatedAt) - moment(b.updatedAt));
+  [...postsData].sort((a, b) => moment(a.createdAt) - moment(b.createdAt));
 
 export const sortByTrending = (postsData) =>
   postsData.filter((data) => data.isTrending);
