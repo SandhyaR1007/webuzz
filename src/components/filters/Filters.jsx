516d046aada88f27f5e2229146fa7bd07717d5aa
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-25 19:45:29 +0530

    update: added dynamic button names

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
-const Filters = () => {
+const Filters = ({ sortBy, setSortBy }) => {
+  const handleClick = (e) => setSortBy(e.target.name);
+  const btns = ["latest", "trending"];
   return (
     <div className="grid grid-cols-2 pb-2">
-      <button className="bg-gray-100 p-3">Latest</button>
-      <button className=" p-3">Trending</button>
+      {btns.map((btnName) => (
+        <button
+          key={btnName}
+          className={`${
+            sortBy == btnName ? "bg-gray-100" : ""
+          } p-3 capitalize transition`}
+          name={btnName}
+          onClick={handleClick}
+        >
+          {btnName}
+        </button>
+      ))}
     </div>
   );
 };
