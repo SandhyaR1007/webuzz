6eb9419ed314d8a5f0a8aa6b79fa3153cd3945e5
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-27 13:48:38 +0530

    update: created dropdown with antd

--- src/components/common/CustomDropdownMenu.jsx
+++ src/components/common/CustomDropdownMenu.jsx
@@ -1,29 +1,18 @@
+import { Dropdown } from "antd";
 import React from "react";
 
-const CustomDropdownMenu = ({ dropdownMenu, setShowDropdown }) => {
+const CustomDropdownMenu = ({ dropdownMenu, icon }) => {
   return (
-    <div
-      className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-      role="menu"
-      aria-orientation="vertical"
-      aria-labelledby="menu-button"
-      tabindex="-1"
+    <Dropdown
+      trigger="click"
+      menu={{
+        items: dropdownMenu,
+      }}
+      placement="bottomLeft"
+      theme="dark"
     >
-      <div className="py-1" role="none">
-        {dropdownMenu.map((item) => (
-          <span
-            role="menuitem"
-            onClick={() => {
-              item.handler();
-              setShowDropdown(false);
-            }}
-            className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-200 cursor-pointer"
-          >
-            {item.label}
-          </span>
-        ))}
-      </div>
-    </div>
+      {icon}
+    </Dropdown>
   );
 };
 
