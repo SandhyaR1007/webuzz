44ee12e0a30b9be79ba99cc19df0f95ae274aeb3
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-28 01:01:04 +0530

    feat: implemented search functionality

---
+++ src/components/filters/Search.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { BsSearch } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { usersSelector } from "../../app/features/usersSlice";
+import { Link, useNavigate } from "react-router-dom";
+import Loader from "../common/Loader";
+const Search = () => {
+  const { usersData } = useSelector(usersSelector);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [foundUsers, setFoundUsers] = useState([]);
+  const [searching, setSearching] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setSearching(true);
+    let id = setTimeout(() => {
+      setFoundUsers(searchUser());
+      setSearching(false);
+    }, 400);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [searchQuery]);
+  const searchUser = () =>
+    searchQuery.length > 0
+      ? usersData.filter(
+          ({ firstName, lastName, username }) =>
+            firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            username.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : [];
+
+  return (
+    <div className="w-60 sm:w-80 relative">
+      <label
+        className={`flex bg-gray-500/10 border border-gray-500 rounded-full  items-center gap-3 py-2  px-3 sm:px-4 w-full`}
+      >
+        <BsSearch />
+        <input
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          type="search"
+          placeholder="Search User"
+          className=" outline-none bg-transparent text-sm w-full"
+        />
+      </label>
+      {searchQuery.length > 0 && (
+        <ul className="border border-gray-100/10 mt-1 absolute  z-1000 bg-[--card-bg] w-full shadow-sm p-1 rounded-lg text-[--primary-text]">
+          {searching ? (
+            <div className="text-center">
+              {" "}
+              <Loader />
+            </div>
+          ) : foundUsers.length === 0 ? (
+            <p className="text-center text-[--primary-text]">No User Found</p>
+          ) : (
+            foundUsers.map(({ profile, username, firstName, lastName }) => (
+              <li
+                className="cursor-pointer hover:bg-black/10 text-sm p-2 border-b border-gray-100/10"
+                onClick={() => {
+                  navigate(`/userProfile/${username}`);
+                  setSearchQuery("");
+                }}
+              >
+                <div className="flex items-center gap-2">
+                  <img
+                    src={profile}
+                    alt={username}
+                    className="w-10 h-10 rounded-lg border border-black object-cover"
+                  />
+                  <div className="flex flex-col">
+                    <h1 className="font-semibold ">
+                      {firstName} {lastName}
+                    </h1>
+
+                    <span className="text-xs text-gray-400">@{username}</span>
+                  </div>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Search;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,25 +2,17 @@ import React, { useState } from "react";
 import { BsSearch, BsFillMoonStarsFill } from "react-icons/bs";
 import { FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
+import Search from "../filters/Search";
 
 const Navbar = () => {
   const [theme, setTheme] = useState(false);
   const dispatch = useDispatch();
 
   return (
-    <nav className="fixed left-0 right-0 top-0 py-4 px-2 xs:px-10 md:px-20  flex items-center justify-between gap-2  z-10  bg-[--card-bg] text-[--primary-text] h-16 shadow-md">
+    <nav className="fixed left-0 right-0 top-0 py-4 px-2 xs:px-10 md:px-20  flex items-center justify-between gap-2  z-2  bg-[--card-bg] text-[--primary-text] h-16 shadow-md">
       <div className="text-3xl text-yellow-500 font-secondary">Webuzz</div>
+      <Search />
       <div className="flex sm:gap-10 sm:pe-5 items-center">
-        <label
-          className={`flex bg-gray-500/10 sm:border border-gray-500 rounded-full  items-center gap-3 py-3 sm:py-2  px-3 sm:px-4  sm:w-80`}
-        >
-          <BsSearch />
-          <input
-            type="search"
-            placeholder="Search"
-            className="hidden sm:block outline-none bg-transparent text-sm"
-          />
-        </label>
         <button
           className={`hidden sm:block text-xl shadow-sm text-[--primary-text] border border-black
             "bg-sky-300 shadow-light"
