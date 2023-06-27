4605ef0382cfdbedd59b9155d75ca8003bb3bf6c
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-27 13:48:51 +0530

    update: created antd modal

--- src/components/common/CustomModal.jsx
+++ src/components/common/CustomModal.jsx
@@ -1,6 +1,4 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
+import { Modal } from "antd";
 
 const CustomModal = ({
   modalComponent,
@@ -8,27 +6,9 @@ const CustomModal = ({
   setShowModal,
   width = 400,
 }) => {
-  const style = {
-    position: "absolute",
-    top: "40%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: width,
-    background: "#fff",
-    borderRadius: "10px",
-    boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-    p: 1.5,
-    zIndex: 1000,
-  };
-
   return (
-    <Modal
-      open={showModal}
-      onClose={() => setShowModal(false)}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>{modalComponent}</Box>
+    <Modal open={showModal} onCancel={() => setShowModal(false)} footer={null}>
+      {modalComponent}
     </Modal>
   );
 };
