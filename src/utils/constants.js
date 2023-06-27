0cd05ba14dff1d54ec87151c30e2aeaea14b22a0
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-27 13:49:53 +0530

    add: avatars list

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const avatars = [
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851183/avatars/vwhq4ge03qcntofpfppp.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/trapmosse2treey1m9db.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/yju5lmlw1p4fgjjyxhi9.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/pbesat0slyx3hqloxcnq.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/m1ryaztaohti1j2jkqum.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851181/avatars/q4znkd8ze0sopzh8eqvb.png",
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/twh5nnmnw4z0seefijej.png",
+];
