a0d5f7edead5c39bcc0c318ee6eb3d27407ac50a
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-28 22:37:10 +0530

    update: fixed spacing

--- src/components/layout/SharedLayout.jsx
+++ src/components/layout/SharedLayout.jsx
@@ -10,7 +10,7 @@ const SharedLayout = () => {
       <Navbar />
       <div className="mt-16">
         <Menubar />
-        <div className="border border-black flex items-start  gap-3 mx-0 sm:ms-[20vw] lg:ms-[20vw] lg:me-[27vw] xs:px-10 min-h-screen  w-full sm:w-[80vw] lg:w-[53vw] bg-[--bg-color]">
+        <div className="border border-black flex items-start  gap-3 mx-0 sm:ms-[100px] md:ms-[250px] lg:me-[27vw] xs:px-10 min-h-screen  w-full sm:w-[80vw] lg:w-[53vw] bg-[--bg-color]">
           <Outlet />
         </div>
         <SuggestedUsers />
