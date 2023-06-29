90df04399df3527d447d15ca71077099b7cb0625
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-06-30 02:11:56 +0530

    update: added timestamp formatter

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const avatars = [
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851183/avatars/vwhq4ge03qcntofpfppp.png",
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/trapmosse2treey1m9db.png",
@@ -7,3 +9,13 @@ export const avatars = [
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851181/avatars/q4znkd8ze0sopzh8eqvb.png",
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/twh5nnmnw4z0seefijej.png",
 ];
+export const formatTimestamp = (timestamp) => {
+  const now = moment();
+  const targetDate = moment(timestamp);
+
+  if (now.diff(targetDate, "hours") < 24) {
+    return targetDate.fromNow();
+  } else {
+    return targetDate.format("Do MMMM, YYYY");
+  }
+};
