ae133cc47614da36d784b98f8481384592b07f9a
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-01 01:38:05 +0530

    update: added theme to antd components

--- src/components/common/CustomDropdownMenu.jsx
+++ src/components/common/CustomDropdownMenu.jsx
@@ -1,18 +1,30 @@
-import { Dropdown } from "antd";
+import { ConfigProvider, Dropdown, theme } from "antd";
 import React from "react";
+import { useSelector } from "react-redux";
+import { themeSelector } from "../../app/features/themeSlice";
 
 const CustomDropdownMenu = ({ dropdownMenu, icon }) => {
+  const { theme: currentTheme } = useSelector(themeSelector);
   return (
-    <Dropdown
-      trigger="click"
-      menu={{
-        items: dropdownMenu,
+    <ConfigProvider
+      theme={{
+        algorithm:
+          currentTheme && currentTheme === "dark"
+            ? theme.darkAlgorithm
+            : theme.defaultAlgorithm,
       }}
-      placement="bottomLeft"
-      theme="dark"
     >
-      {icon}
-    </Dropdown>
+      <Dropdown
+        trigger="click"
+        menu={{
+          items: dropdownMenu,
+        }}
+        placement="bottomLeft"
+        theme="dark"
+      >
+        {icon}
+      </Dropdown>
+    </ConfigProvider>
   );
 };
 
--- src/components/common/CustomModal.jsx
+++ src/components/common/CustomModal.jsx
@@ -1,4 +1,6 @@
-import { Modal } from "antd";
+import { ConfigProvider, Modal, theme } from "antd";
+import { useSelector } from "react-redux";
+import { themeSelector } from "../../app/features/themeSlice";
 
 const CustomModal = ({
   modalComponent,
@@ -6,10 +8,24 @@ const CustomModal = ({
   setShowModal,
   width = 400,
 }) => {
+  const { theme: currentTheme } = useSelector(themeSelector);
   return (
-    <Modal open={showModal} onCancel={() => setShowModal(false)} footer={null}>
-      {modalComponent}
-    </Modal>
+    <ConfigProvider
+      theme={{
+        algorithm:
+          currentTheme && currentTheme === "dark"
+            ? theme.darkAlgorithm
+            : theme.defaultAlgorithm,
+      }}
+    >
+      <Modal
+        open={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={null}
+      >
+        {modalComponent}
+      </Modal>
+    </ConfigProvider>
   );
 };
 
