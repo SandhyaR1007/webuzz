8ee3a739023452cd3b1ac3a7abebd9568be78751
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-07-01 01:07:07 +0530

    update: changed styles and responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { authSelector, userLogin } from "../app/features/authSlice";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
+import { loginImg } from "../utils/constants";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -28,49 +29,51 @@ const Login = () => {
   }, [encodedToken]);
 
   return (
-    <div className="w-full flex align-center justify-center  h-screen bg-[--bg-color]">
-      <div className="w-full   flex flex-col items-center  justify-center gap-5 px-5">
+    <div className="w-full flex align-center justify-center  h-screen bg-[#17181c]">
+      <div className="w-full sm:w-1/2   flex flex-col items-center  justify-center gap-5 px-5">
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col  gap-3  py-5 px-10 rounded-lg border shadow-md  w-full xs:w-96"
+          className="flex flex-col  gap-3  py-5 px-5 lg:px-14 rounded-lg    w-full xs:w-2/3 sm:w-full "
         >
-          <h1 className="font-secondary text-3xl text-center">Webuzz</h1>
+          <h1 className="text-blue-400 font-secondary text-5xl text-center drop-shadow-lg">
+            Webuzz
+          </h1>
+          <p className="text-gray-300 text-center">
+            Welcome back to Webuzz, where your journey continues.
+          </p>
 
-          <label htmlFor="username font-xl">
-            <span className="text-sm  text-[--primary-text]">Username</span>
+          <label htmlFor="username" className="text-xl mt-5">
+            <span className="text-gray-50 mb-1 text-sm">Username</span>
             <input
               id="username"
               type="text"
               name="username"
               value={userInfo.username}
               onChange={handleInputChange}
-              className="px-2 py-1 bg-transparent w-full outline-none rounded-lg "
+              className="border border-gray-600 px-2 text-sm py-1.5 sm:py-2.5 bg-[#1e1f23] text-gray-50 w-full outline-none rounded-lg "
             />
           </label>
           <label htmlFor="password">
-            <span className="text-sm font-semibold text-gray-600">
-              {" "}
-              Password
-            </span>
+            <span className="text-gray-50 mb-1 text-sm"> Password</span>
             <input
               id="password"
               type="password"
               name="password"
               value={userInfo.password}
               onChange={handleInputChange}
-              className="px-2 py-1 border-rounded-dark w-full outline-none rounded-lg "
+              className="focus:bg-[#1e1f23] border border-gray-600 px-2 text-sm py-1.5 sm:py-2.5 bg-[#1e1f23] text-gray-50 w-full outline-none rounded-lg "
             />
           </label>
           <section className="flex flex-col gap-3">
             <button
-              className="shadow-light mt-3 px-2 py-1 border-rounded-dark bg-purple font-semibold"
+              className="mt-3 px-2 py-2 border-rounded-dark bg-blue-400 font-semibold"
               type="submit"
               disabled={loggingIn}
             >
-              Login
+              {loggingIn ? "Logging In..." : "Login"}
             </button>
             <button
-              className="shadow-light px-2 py-1 border-rounded-dark bg-pink font-semibold"
+              className=" border px-2 py-2 border-rounded-dark bg-purple-400 font-semibold"
               type="submit"
               disabled={loggingIn}
               onClick={() => {
@@ -82,14 +85,24 @@ const Login = () => {
             >
               Login as Guest
             </button>
-            <p className="text-center">
+            <p className="text-center text-gray-300">
               Don't have an Account?{" "}
-              <Link to="/signup" className="font-semibold hover:underline">
+              <Link
+                to={loggingIn ? "" : "/signup"}
+                className="font-semibold hover:underline text-blue-400"
+              >
                 Sign Up
               </Link>
             </p>
           </section>
         </form>
+      </div>
+      <div className="w-3/4 hidden sm:block">
+        <img
+          src={loginImg}
+          alt="loginImg"
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,6 +9,10 @@ export const avatars = [
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851181/avatars/q4znkd8ze0sopzh8eqvb.png",
   "https://res.cloudinary.com/dwdcib0hv/image/upload/v1687851182/avatars/twh5nnmnw4z0seefijej.png",
 ];
+export const loginImg =
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1688151995/samples/webuzz/ei6jcbeyu2fgpklm3uxv.jpg";
+export const signupImg =
+  "https://res.cloudinary.com/dwdcib0hv/image/upload/v1688151995/samples/webuzz/pz2yn0rx8qijlygv6f1u.png";
 export const formatTimestamp = (timestamp) => {
   const now = moment();
   const targetDate = moment(timestamp);
